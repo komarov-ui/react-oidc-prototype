10256d9e7cb71452e43ed4cc40f2c0e0125c52d7
Sergei Komarov <sergei.komarov.qubership@gmail.com>
2025-02-20 15:51:18 +0400

    Fixed approach preventing double requests for tokens

--- src/auth/useToken.js
+++ src/auth/useToken.js
@@ -3,21 +3,22 @@ import { useLocalStorage } from 'react-use';
 import { API_GET_REQUEST_TOKEN } from '../consts/api';
 import { AUTH_KEY, AUTH_LOADING_KEY, AUTH_ORIGIN_PAGE } from '../consts/auth';
 
-export function useToken(code) {
+export function useToken(authCode) {
   const [, setAuth] = useLocalStorage(AUTH_KEY);
   const [, , clearAuthLoading] = useLocalStorage(AUTH_LOADING_KEY)
   const [originPage, , clearOriginPage] = useLocalStorage(AUTH_ORIGIN_PAGE)
 
-  const isAuthorizationCodeUsed = useRef(false)
+  const isAuthorizationCodeUsed = useRef(null)
 
   useEffect(() => {
-    if (isAuthorizationCodeUsed.current) {
+    const savedAuthCode = isAuthorizationCodeUsed.current
+    if (savedAuthCode && savedAuthCode === authCode) {
       return;
     }
-    isAuthorizationCodeUsed.current = true;
+    isAuthorizationCodeUsed.current = authCode;
 
     // Request access token and refresh token
-    fetch(`${API_GET_REQUEST_TOKEN}?code=` + encodeURIComponent(code), {
+    fetch(`${API_GET_REQUEST_TOKEN}?code=` + encodeURIComponent(authCode), {
       credentials: 'include', // Include cookies
     }).then(response => {
       return response.json();
@@ -31,5 +32,5 @@ export function useToken(code) {
         location.href = originPage
       }
     }).catch(error => console.error(error));
-  }, [clearAuthLoading, clearOriginPage, code, originPage, setAuth])
+  }, [clearAuthLoading, clearOriginPage, authCode, originPage, setAuth])
 }
